14acf5c9b07872f0689f3ee19bcf57687177de62
Desmond Obisi <desmondcutest@yahoo.com>
2024-09-17 18:25:24 +0100

    chore: swagger API documentation

---
+++ docs/index.ts
@@ -0,0 +1,37 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import { port } from '../src/config/app.config.js';
+
+const swaggerOptions = {
+ definition: {
+  openapi: '3.0.0',
+  info: {
+   title: 'Banking Demo API',
+   version: '1.0.0',
+   description: 'API documentation for the Banking Demo application',
+  },
+  servers: [
+   {
+    url: `http://localhost:${port}`,
+   },
+  ],
+  components: {
+   securitySchemes: {
+    BearerAuth: {
+     type: 'http',
+     scheme: 'bearer',
+     bearerFormat: 'JWT',
+    },
+   },
+  },
+  security: [
+   {
+    BearerAuth: [],
+   },
+  ],
+ },
+ apis: ['./docs/specs/*.yml'], // Path to the API routes
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export default swaggerSpec;
